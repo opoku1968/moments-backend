b35386acb4832f8e46cc42f79c22a4ea2967eedb
Prince Wiliam  Opoku Mensah <kofipos@yahoo.com>
2024-01-25 21:10:03 +0000

    updated files

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -40,7 +40,7 @@ REST_FRAMEWORK = {
 }
 if 'DEV' not in os.environ:
     REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
-        'rest_framework.renders.JSONRenderer',
+        'rest_framework.renderers.JSONRenderer',
     ]
 REST_USE_JWT = True
 JWT_AUTH_SECURE = True 
