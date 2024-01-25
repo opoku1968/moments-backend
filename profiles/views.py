b81ca411c36045ef6886d772fcde9c494eb77e44
Prince Wiliam  Opoku Mensah <kofipos@yahoo.com>
2024-01-25 10:34:26 +0000

    added permisinos

--- profiles/views.py
+++ profiles/views.py
@@ -4,33 +4,35 @@ from .models import Profile
 from rest_framework import status
 from .serializers import ProfileSerializer
 from django.http import Http404
-
+from drf_api.permissions import isOwnerOrReadOnly
 
 
 class ProfileList(APIView):
     def get(self,request):
         profiles = Profile.objects.all()
-        serializers = ProfileSerializer(profiles,many=True)
+        serializers = ProfileSerializer(profiles,many=True,context={'request':request})
         return Response(serializers.data)
 
 
 class ProfileDetail(APIView):
     serializer_class = ProfileSerializer
+    permission_classes = [isOwnerOrReadOnly]
     def get_object(self,pk):
         try:
             profile = Profile.objects.get(pk=pk)
+            self.check_object_permissions(self.request,profile)
             return profile
         except Profile.DoesNotExist:
             raise Http404
 
     def get(self,request,pk):
         profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile)
+        serializer = ProfileSerializer(profile,context={'request':request})
         return Response(serializer.data)
 
     def put(self,request,pk):
         profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile,data=request.data)
+        serializer = ProfileSerializer(profile,data=request.data,context={'request':request})
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
