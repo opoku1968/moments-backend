61f709a8c31a5700b3205596f89e12d655e2a735
Prince Wiliam  Opoku Mensah <kofipos@yahoo.com>
2024-01-25 14:48:18 +0000

    handled duplicate likes

---
+++ likes/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from likes.models import Like
+from django.db import IntegrityError
+
+class LikeSerializer(serializers.ModelSerializer):
+
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Like
+        fields = ['id', 'created_at', 'owner', 'post']
+
+    def create(self,validated_data):
+        try:
+            return super().create(validate_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'detail': 'possible duplicate'
+            })
+
+    
