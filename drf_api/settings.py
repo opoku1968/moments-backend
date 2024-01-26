1c6950a42fdc3199d8a9dcffa7c531788e7d62cc
Prince Wiliam  Opoku Mensah <kofipos@yahoo.com>
2024-01-26 20:08:01 +0000

    updated files

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -92,10 +92,10 @@ INSTALLED_APPS = [
     'likes',
     'followers',
 ]
-# CORS_ALLOWED_ORIGINS = [
-#    'http://localhost:3000'
-# ]
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+   'http://localhost:3000'
+]
+# CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOW_CREDENTIALS = True
 
