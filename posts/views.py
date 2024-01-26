11e42334b620b8e8c31094c9fc315f6b545da32f
Prince Wiliam  Opoku Mensah <kofipos@yahoo.com>
2024-01-26 12:49:01 +0000

    updated files

--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,9 @@
 from django.db.models import Count
 from rest_framework import generics, permissions, filters
-from drf_api.permissions import isOwnerOrReadOnly
+from django_filters.rest_framework import DjangoFilterBackend
+from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Post
 from .serializers import PostSerializer
-from django_filters.rest_framework import DjangoFilterBackend
 
 
 class PostList(generics.ListCreateAPIView):
@@ -22,18 +22,15 @@ class PostList(generics.ListCreateAPIView):
         filters.SearchFilter,
         DjangoFilterBackend,
     ]
-    search_fields = [
-        'owner__username',
-        'title',
-
-    ]
-    filterset_fields =[
+    filterset_fields = [
         'owner__followed__owner__profile',
         'likes__owner__profile',
         'owner__profile',
-
     ]
-
+    search_fields = [
+        'owner__username',
+        'title',
+    ]
     ordering_fields = [
         'likes_count',
         'comments_count',
@@ -49,7 +46,7 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     Retrieve a post and edit or delete it if you own it.
     """
     serializer_class = PostSerializer
-    permission_classes = [isOwnerOrReadOnly]
+    permission_classes = [IsOwnerOrReadOnly]
     queryset = Post.objects.annotate(
         likes_count=Count('likes', distinct=True),
         comments_count=Count('comment', distinct=True)
