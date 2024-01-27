23220b4483e660d256142be6012cb543fa95c926
Prince Wiliam  Opoku Mensah <kofipos@yahoo.com>
2024-01-27 16:50:22 +0000

    updated files

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -5,6 +5,7 @@ from followers.models import Follower
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    following_id = serializers.SerializerMethodField()
     posts_count = serializers.ReadOnlyField()
     followers_count = serializers.ReadOnlyField()
     following_count = serializers.ReadOnlyField()
@@ -26,6 +27,6 @@ class ProfileSerializer(serializers.ModelSerializer):
         model = Profile
         fields = [
             'id', 'owner', 'created_at', 'updated_at', 'name',
-            'content', 'image', 'is_owner', 'posts_count',
+            'content', 'image', 'is_owner','following_id', 'posts_count',
             'following_count', 'followers_count',
         ]
