6c2b3915528d5f92c6dea00941dac15365f4f68d
Prince Wiliam  Opoku Mensah <kofipos@yahoo.com>
2024-02-08 11:03:29 +0000

    new update

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -93,7 +93,7 @@ INSTALLED_APPS = [
     'followers',
 ]
 CORS_ALLOWED_ORIGINS = [
-   'https://moments-frontend-21e28627c0cd.herokuapp.com/',
+   'https://moments-frontend-21e28627c0cd.herokuapp.com',
    'http://localhost:3000',
 
 ]
